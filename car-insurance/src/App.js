dbd86785e8838bfe05502e0da61f82eb2082f267
khareenp <khareenp1@gmail.com>
2022-08-07 23:46:31 -0400

    updated compnent

--- car-insurance/src/App.js
+++ car-insurance/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className=" flex flex-col space-y-4 w-full h-screen bg-slate-600">
+      <h2 className="text-4xl">Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
--- car-insurance/src/components/ExpenseItem.jsx
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -1,9 +1,16 @@
 function ExpenseItem() {
   return (
-    <div>
-      <div>August 7th 2022</div>
-      <h2 className="title text-3xl">Car Insurance</h2>
-      <div>$294.67</div>
+    <div className=" flex justify-between bg-slate-400 p-4 items-center rounded-lg">
+      <div className="flex space-x-8 items-center">
+        <div className=" text-2xl">August 7th 2022</div>
+        <h2 className="title text-3xl text-slate-50 font-bold">
+          Car Insurance
+        </h2>
+      </div>
+
+      <div className=" bg-indigo-500 text-2xl p-4 border-2 rounded-2xl text-slate-50">
+        $294.67
+      </div>
     </div>
   );
 }
