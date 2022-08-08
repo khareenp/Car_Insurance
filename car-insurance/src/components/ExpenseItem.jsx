e5a8b2d589d4de5a1d3360deb9e0c94da18cefcf
khareenp <khareenp1@gmail.com>
2022-08-07 23:48:24 -0400

    update css

--- car-insurance/src/components/ExpenseItem.jsx
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -1,14 +1,14 @@
 function ExpenseItem() {
   return (
-    <div className=" flex justify-between bg-slate-400 p-4 items-center rounded-lg">
+    <div className="expense-item flex justify-between bg-slate-400 p-4 items-center rounded-lg">
       <div className="flex space-x-8 items-center">
-        <div className=" text-2xl">August 7th 2022</div>
-        <h2 className="title text-3xl text-slate-50 font-bold">
+        <div className="expense-item__date text-2xl">August 7th 2022</div>
+        <h2 className="expense-item__description text-3xl text-slate-50 font-bold">
           Car Insurance
         </h2>
       </div>
 
-      <div className=" bg-indigo-500 text-2xl p-4 border-2 rounded-2xl text-slate-50">
+      <div className="expense-item__price bg-indigo-500 text-2xl p-4 border-2 rounded-2xl text-slate-50">
         $294.67
       </div>
     </div>
