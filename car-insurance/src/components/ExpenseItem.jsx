05de07d449a906df029579811a9336b1897c7a6b
khareenp <khareenp1@gmail.com>
2022-08-08 00:22:56 -0400

    passed props to component expense item

--- car-insurance/src/components/ExpenseItem.jsx
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -1,21 +1,17 @@
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 8, 8);
-  const expenseTitle = "Car Insurance";
-  let expenseAmount = 294.75;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item flex justify-between bg-slate-400 p-4 items-center rounded-lg">
       <div className="flex space-x-8 items-center">
         <div className="expense-item__date text-2xl">
-          {expenseDate.toISOString()}
+          {props.date.toISOString()}
         </div>
         <h2 className="expense-item__description text-3xl text-slate-50 font-bold">
-          {expenseTitle}
+          {props.title}
         </h2>
       </div>
 
       <div className="expense-item__price bg-indigo-500 text-2xl p-4 border-2 rounded-2xl text-slate-50">
-        ${expenseAmount}
+        ${props.amount}
       </div>
     </div>
   );
