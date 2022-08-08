90594d3258df3402e5027e336bb57b67648b87bf
khareenp <khareenp1@gmail.com>
2022-08-08 00:29:12 -0400

    update props

--- car-insurance/src/App.js
+++ car-insurance/src/App.js
@@ -6,19 +6,25 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 7, 14).toISOString(),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12).toISOString(),
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 2, 28).toISOString(),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
+      date: new Date(2021, 2, 28).toISOString(),
     },
   ];
 
--- car-insurance/src/components/ExpenseItem.jsx
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -2,9 +2,7 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item flex justify-between bg-slate-400 p-4 items-center rounded-lg">
       <div className="flex space-x-8 items-center">
-        <div className="expense-item__date text-2xl">
-          {props.date.toISOString()}
-        </div>
+        <div className="expense-item__date text-2xl">{props.date}</div>
         <h2 className="expense-item__description text-3xl text-slate-50 font-bold">
           {props.title}
         </h2>
