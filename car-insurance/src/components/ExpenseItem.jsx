ca69d9364c228d71a0ce56c7f33dae532cde2465
khareenp <khareenp1@gmail.com>
2022-08-08 00:43:10 -0400

    update with props

--- car-insurance/src/components/ExpenseItem.jsx
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -1,8 +1,15 @@
 function ExpenseItem(props) {
+  const month = props.date.toLocaleDateString("en-US", { month: "long" });
+  const year = props.date.toLocaleDateString("en-US", { year: "numeric" });
+  const day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
   return (
     <div className="expense-item flex justify-between bg-slate-400 p-4 items-center rounded-lg">
       <div className="flex space-x-8 items-center">
-        <div className="expense-item__date text-2xl">{props.date}</div>
+        <div className="expense-item__date text-2xl">
+          <div>{month}</div>
+          <div>{year}</div>
+          <div>{day}</div>
+        </div>
         <h2 className="expense-item__description text-3xl text-slate-50 font-bold">
           {props.title}
         </h2>
