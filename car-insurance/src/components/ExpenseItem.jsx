6ef3c3cbe54df3d39168dd9293f43e266fb55f6d
khareenp <khareenp1@gmail.com>
2022-08-08 00:05:25 -0400

    update

--- car-insurance/src/components/ExpenseItem.jsx
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -6,7 +6,9 @@ function ExpenseItem() {
   return (
     <div className="expense-item flex justify-between bg-slate-400 p-4 items-center rounded-lg">
       <div className="flex space-x-8 items-center">
-        <div className="expense-item__date text-2xl">{expenseTitle}</div>
+        <div className="expense-item__date text-2xl">
+          {expenseDate.toISOString()}
+        </div>
         <h2 className="expense-item__description text-3xl text-slate-50 font-bold">
           {expenseTitle}
         </h2>
