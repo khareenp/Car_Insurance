32f9d1f76ab3c413128db1639779385d4806fd95
khareenp <khareenp1@gmail.com>
2022-08-07 23:36:52 -0400

    update

---
+++ car-insurance/src/components/ExpenseItem.jsx
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>August 7th 2022</div>
+      <h2 className="title text-3xl">Car Insurance</h2>
+      <div>$294.67</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
